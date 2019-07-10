327a6ae0e5bb7f67bcfee2a2cacd4e8b74acbcfc
buck <coderbuck@163.com>
2019-07-10 19:33:39 +0800

    list_item_garden_planting

--- app/src/main/java/me/buck/sunflower_java/viewmodels/PlantDetailViewModel.java
+++ app/src/main/java/me/buck/sunflower_java/viewmodels/PlantDetailViewModel.java
@@ -3,6 +3,7 @@ package me.buck.sunflower_java.viewmodels;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import me.buck.sunflower_java.data.GardenPlanting;
 import me.buck.sunflower_java.data.GardenPlantingRepository;
 import me.buck.sunflower_java.data.Plant;
 import me.buck.sunflower_java.data.PlantRepository;
@@ -12,16 +13,34 @@ import me.buck.sunflower_java.data.PlantRepository;
  */
 public class PlantDetailViewModel extends ViewModel {
 
-    private PlantRepository mPlantRepository;
+    private PlantRepository          mPlantRepository;
     private GardenPlantingRepository mGardenPlantingRepository;
-    private String plantId;
+    private String                   mPlantId;
 
     public PlantDetailViewModel(PlantRepository plantRepository, GardenPlantingRepository gardenPlantingRepository, String plantId) {
         mPlantRepository = plantRepository;
         mGardenPlantingRepository = gardenPlantingRepository;
-        this.plantId = plantId;
+        this.mPlantId = plantId;
+
+        init();
     }
 
     private LiveData<Boolean> mIsPlant;
     private LiveData<Plant>   mPlantLiveData;
+
+    private void init() {
+        LiveData<GardenPlanting> gardenPlantingForPlant = mGardenPlantingRepository.getGardenPlantingForPlant(mPlantId);
+        //gardenPlantingForPlant.
+        // TODO: 2019/7/10  
+    }
+
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+
+
+
+
 }
