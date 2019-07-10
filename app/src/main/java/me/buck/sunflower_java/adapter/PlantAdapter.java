844c244525d851294a94b5c04140742697ea5d91
buck <coderbuck@163.com>
2019-07-10 12:32:55 +0800

    plant_adapter

--- app/src/main/java/me/buck/sunflower_java/adapter/PlantAdapter.java
+++ app/src/main/java/me/buck/sunflower_java/adapter/PlantAdapter.java
@@ -3,6 +3,8 @@ package me.buck.sunflower_java.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
@@ -33,14 +35,29 @@ public class PlantAdapter extends ListAdapter<Plant, PlantAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull PlantAdapter.ViewHolder holder, int position) {
+        Plant plant = getItem(position);
+        holder.itemView.setTag(plant);
+        holder.itemView.setOnClickListener(createOnClickListener(plant.getPlantId()));
+    }
 
+    private View.OnClickListener createOnClickListener(String plantId) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView img;
+        TextView  title;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            img = itemView.findViewById(R.id.plant_item_image);
+            title = itemView.findViewById(R.id.plant_item_title);
         }
     }
 
