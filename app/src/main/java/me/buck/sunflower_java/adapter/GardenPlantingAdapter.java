a88f69583cfadf9cf9be4a0a652abf153f3295c4
buck <coderbuck@163.com>
2019-07-10 14:45:54 +0800

    list_item_garden_planting

--- app/src/main/java/me/buck/sunflower_java/adapter/GardenPlantingAdapter.java
+++ app/src/main/java/me/buck/sunflower_java/adapter/GardenPlantingAdapter.java
@@ -3,14 +3,20 @@ package me.buck.sunflower_java.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.Objects;
 
+import me.buck.sunflower_java.GardenFragmentDirections;
 import me.buck.sunflower_java.R;
 import me.buck.sunflower_java.data.PlantAndGardenPlantings;
 
@@ -19,17 +25,14 @@ import me.buck.sunflower_java.data.PlantAndGardenPlantings;
  */
 public class GardenPlantingAdapter extends ListAdapter<PlantAndGardenPlantings, GardenPlantingAdapter.Holder> {
 
-    private static InnerDiffCallback sDiffCallback = new InnerDiffCallback();
-
     public GardenPlantingAdapter() {
-        super(sDiffCallback);
+        super(InnerDiffCallback.create());
     }
 
     @NonNull
     @Override
     public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                // TODO: 2019/7/9 list item
                 .inflate(R.layout.list_item_garden_planting, parent, false);
         return new Holder(view);
     }
@@ -38,11 +41,29 @@ public class GardenPlantingAdapter extends ListAdapter<PlantAndGardenPlantings, 
     public void onBindViewHolder(@NonNull Holder holder, int position) {
         PlantAndGardenPlantings item = getItem(position);
         holder.itemView.setTag(item);
+        holder.itemView.setOnClickListener(v -> {
+            GardenFragmentDirections.ActionGardenFragmentToPlantDetailFragment fragment
+                    = GardenFragmentDirections.actionGardenFragmentToPlantDetailFragment(item.getPlant().getPlantId());
+            Navigation.findNavController(v).navigate(fragment);
+        });
+
+        Glide.with(holder.imageView)
+                .load(item.getPlant().getImageUrl())
+                .into(holder.imageView);
+
+        // TODO: 2019/7/10  plantDate  waterDate
+
     }
 
     class Holder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView  plantDate, waterDate;
+
         public Holder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            plantDate = itemView.findViewById(R.id.plant_date);
+            waterDate = itemView.findViewById(R.id.water_date);
         }
     }
 
@@ -61,7 +82,10 @@ public class GardenPlantingAdapter extends ListAdapter<PlantAndGardenPlantings, 
                     oldItem.getPlant(),
                     newItem.getPlant()
             );
+        }
 
+        public static InnerDiffCallback create() {
+            return new InnerDiffCallback();
         }
     }
 
