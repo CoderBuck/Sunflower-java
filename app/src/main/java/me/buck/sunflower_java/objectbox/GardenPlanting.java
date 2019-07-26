30aca1cc354fe53b8bba71036094a456b51d03e4
buck <coderbuck@163.com>
2019-07-26 13:06:27 +0800

    objectbox init

---
+++ app/src/main/java/me/buck/sunflower_java/objectbox/GardenPlanting.java
@@ -0,0 +1,14 @@
+package me.buck.sunflower_java.objectbox;
+
+import io.objectbox.annotation.Entity;
+
+/**
+ * Created by gwf on 2019/7/26
+ */
+@Entity
+public class GardenPlanting {
+
+    private long id;
+
+
+}
