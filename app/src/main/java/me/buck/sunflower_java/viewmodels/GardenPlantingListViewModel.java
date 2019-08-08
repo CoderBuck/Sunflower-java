7b524394c47b5ec2b44d28c37aea5e5044928e21
buck <coderbuck@163.com>
2019-08-08 16:44:31 +0800

    tmp

--- app/src/main/java/me/buck/sunflower_java/viewmodels/GardenPlantingListViewModel.java
+++ app/src/main/java/me/buck/sunflower_java/viewmodels/GardenPlantingListViewModel.java
@@ -1,17 +1,18 @@
 package me.buck.sunflower_java.viewmodels;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
 import com.annimon.stream.Stream;
 
-import java.util.Iterator;
 import java.util.List;
 
 import me.buck.sunflower_java.data.GardenPlanting;
 import me.buck.sunflower_java.data.GardenPlantingRepository;
 import me.buck.sunflower_java.data.PlantAndGardenPlantings;
+import me.buck.sunflower_java.objectbox.box.GardenPlantingBox;
 
 /**
  * Created by gwf on 2019/6/19
@@ -24,11 +25,18 @@ public class GardenPlantingListViewModel extends ViewModel {
         mRepository = repository;
     }
 
-    public LiveData<List<GardenPlanting>> getGardenPlantings() {return mRepository.getGardenPlantings();}
+    public LiveData<List<GardenPlanting>> getGardenPlantings() {
+        //MutableLiveData<List<me.buck.sunflower_java.objectbox.entity.GardenPlanting>> liveData = new MutableLiveData<>();
+        //List<me.buck.sunflower_java.objectbox.entity.GardenPlanting> plantings = GardenPlantingBox.getGardenPlantings();
+        //liveData.setValue(plantings);
+
+        return mRepository.getGardenPlantings();
+    }
 
     public LiveData<List<PlantAndGardenPlantings>> getPlantAndGardenPlantings() {
         return Transformations.map(mRepository.getPlantAndGardenPlantings(), plantings -> Stream.of(plantings)
                 .filter(it -> !it.getGardenPlantings().isEmpty())
                 .toList());
     }
+
 }
